d199ea4638b01f92745073b32b9168befaf3ecb3
anandukb <kbanandu@applab.qa>
2026-05-21 15:37:32 +0530

    build fix

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import { Code2, Atom, LayoutTemplate, Server } from 'lucide-react';
+// import { Code2, Atom, LayoutTemplate, Server } from 'lucide-react';
+import { Atom, Server } from 'lucide-react';
 import './LandingPage.css';
 
 const LandingPage = () => {
